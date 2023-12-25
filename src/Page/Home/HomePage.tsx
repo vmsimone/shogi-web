7a0aa3a2d6f5e46fecfa9ba04ab48cc7421ea3fc
Vincent Simone <vsimone@protonmail.com>
2023-12-24 23:54:44 -0700

    correct home page structure and update styling

--- src/Page/Home/HomePage.tsx
+++ src/Page/Home/HomePage.tsx
@@ -9,14 +9,14 @@ const HomePage = () => {
     <div>
       <main role="main" aria-live="assertive">
         <h1>Welcome to the United States Shogi Federation!</h1>
+        <section id="about">
+          <h2>About</h2>
+          <p>
+            While not the most popular game in the USA, this exciting, traditional strategy game from Japan has had fans from the United States since the early 1990s! We wanted to bring some of that excitement into the modern day, make it easier for experienced players to find each other, and offer new players and interested prospects an easy way to find shogi clubs and events in their local area. While not an official organization, we hope our efforts can help ease communication and make games more accessible to everyone. <br />
+            Scroll down or use the links at the top to find a club, see the latest information, or learn more about shogi!
+          </p>
+        </section>
       </main>
-      <section id="about">
-        <h2>About</h2>
-        <p>
-          While not the most popular game in the USA, this exciting, traditional strategy game from Japan has had fans from the United States since the early 1990s! We wanted to bring some of that excitement into the modern day, make it easier for experienced players to find each other, and offer new players and interested prospects an easy way to find shogi clubs and events in their local area. While not an official organization, we hope our efforts can help ease communication and make games more accessible to everyone. <br />
-          Scroll down or use the links at the top to find a club, see the latest information, or learn more about shogi!
-        </p>
-      </section>
     </div>
   )
 }
